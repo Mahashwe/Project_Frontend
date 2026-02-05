ca1d46a348cafe7105b94483dafdefcc11e41249
Mahashwe <maha.tomholland@gmail.com>
2026-02-05 11:32:30 +0530

    Implement cart functionality: add "Add to cart" buttons with event handling and style updates for cart buttons and overlay

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function toggleHeart(heart) {
         heart.src = 'images/heart.png';
     }
 }
+
+let cart = ""
+function add_to_cart(event) {
+    let item = event.target.closest(".card").querySelector("h3").textContent; 
+    alert(item + " is added to your cart!"); 
+    cart += item + "\n";
+    console.log(cart);
+}
+
+function openCart() {
+    document.getElementById("cartOverlay").classList.add("active");
+  }
+
+function closeCart() {
+    document.getElementById("cartOverlay").classList.remove("active");
+  }
